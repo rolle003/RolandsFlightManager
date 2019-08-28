7434507b3133d36e33884023f9fc80ebecb9ade6
Roland Stünkel <rolle0514@yahoo.se>
2019-08-28 14:50:39 +0200

    start version for git

---
+++ domain/src/main/java/Booking.java
@@ -0,0 +1,14 @@
+public class Booking {
+
+    public String bookingId;
+
+    public String customerNo;
+
+    public String flightNo;
+
+    public String ticketType;
+
+    public String menuCode;
+
+    public Integer
+}
